7557ad73cbab9cbac4154bfc0db746e2aa93440e
Carly <carly.jenkinson65@googlemail.com>
2020-12-20 20:51:13 +0000

    Can delete ingredient from service.

--- src/ingredientsService.ts
+++ src/ingredientsService.ts
@@ -1,6 +1,6 @@
 import executeQuery from "./db"
 
-interface Ingredients{
+interface Ingredients {
   ingredient_name: string;
   quantity: number;
   unit: string;
@@ -10,14 +10,14 @@ interface IngredientsSaved extends Ingredients {
   id: string;
 }
 
-export async function listIngredients() : Promise < IngredientsSaved[] >{
+export async function listIngredients(): Promise<IngredientsSaved[]> {
   const results = await executeQuery('SELECT * FROM current_stock', []);
-  return results.rows.map( (row:any) =>({id: row.id, ingredient_name: row.ingredient_name, quantity: row.quantity, unit: row.unit}))
+  return results.rows.map((row: any) => ({ id: row.id, ingredient_name: row.ingredient_name, quantity: row.quantity, unit: row.unit }))
 }
 
 export async function addIngredient(ingredient: Ingredients): Promise<IngredientsSaved> {
   const result = await executeQuery('INSERT INTO current_stock(ingredient_name, quantity, unit, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *', [ingredient.ingredient_name, ingredient.quantity, ingredient.unit, new Date(), new Date()]);
-  return {id: result.rows[0].id, ingredient_name: result.rows[0].ingredient_name, quantity: result.rows[0].quantity, unit: result.rows[0].unit};
+  return { id: result.rows[0].id, ingredient_name: result.rows[0].ingredient_name, quantity: result.rows[0].quantity, unit: result.rows[0].unit };
 }
 
 export async function deleteIngredient(id: String): Promise<void> {
